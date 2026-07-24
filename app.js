a18383175d4abcd4822b074f6b768b2eb27f67a0
huangyilong1337 <huangyilong1337@gmail.com>
2026-07-24 09:34:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-window.funds = [
-    {
-        code: "161129",
-        name: "原油LOF",
-        premium: 1.82,
-        turnover: "5.8亿",
-        limit: "100元",
-        settlement: "T+3",
-        score: "★★★★★"
-    },
-    {
-        code: "160416",
-        name: "黄金LOF",
-        premium: 0.95,
-        turnover: "2.4亿",
-        limit: "暂停申购",
-        settlement: "T+2",
-        score: "★★★☆☆"
-    }
-];
+function updateTime() {
+    const now = new Date();
+
+    document.getElementById("time").innerText =
+        now.toLocaleString("zh-CN");
+}
+
+setInterval(updateTime,1000);
+updateTime();
+
+const table = document.getElementById("fundTable");
+
+funds.forEach(fund => {
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${fund.code}</td>
+        <td>${fund.name}</td>
+        <td>${fund.premium}%</td>
+        <td>${fund.turnover}</td>
+        <td>${fund.limit}</td>
+        <td>${fund.settlement}</td>
+        <td>${fund.score}</td>
+    `;
+
+    table.appendChild(row);
+
+});
